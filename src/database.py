160fd125f5c31f41cfb1665157a2ee82ea9c78d7
NcVillalobos <44530847+NcVillalobos@users.noreply.github.com>
2019-03-03 11:40:03 +0000

    Update database.py

--- src/database.py
+++ src/database.py
@@ -4,25 +4,25 @@ class Database():
     #Define Database class, this class contains methods used to create a database to store users login data.
 
     def __init__(self,database_name):
-        #This method will inizialise the Database class. An argument containing the database name is passed to the class.
+        # This method will inizialise the Database class. An argument containing the database name is passed to the class.
         
         self.database = sqlite3.connect(database_name) #create/connect the database
         self.cursor = self.database.cursor()
 
     def create_table(self):
-        #It creates the table usernames
+        # Creates the table 'usernames'
         
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS usernames (Name text, Surname text, Email text, Username text, Password text)''') 
         self.database.commit()
 
     def insert_row(self,name,surname,username,password):
-        #Insert a row into the usernames database. It does not check whether the arguments are valid.
+        # Insert a row into the usernames database. It does not check whether the arguments are valid.
 
         self.cursor.execute('''INSERT INTO usernames (Name,Surname,Username,Password) values (?,?,?,?)''',(name,surname,username,password))
         self.database.commit()
         
     def retrieve_user(self,username):
-        #It retrieve a user from the database. The method received an username as an argument.
+        # Retrieves a user from the database. The method receives an username as an argument.
 
         self.data = self.cursor.execute('''SELECT * FROM usernames WHERE username = ?''', (username,))
         for row in self.data:
